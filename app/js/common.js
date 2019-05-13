894e6e563239878088955ec0b3ad69c8602d9e34
Artem Sizonenko <artem.sizonenko@yanpix.com>
2019-05-13 18:45:21 +0300

    mobile menu

--- app/js/common.js
+++ app/js/common.js
@@ -2,6 +2,22 @@ $(function() {
 
 	// Custom JS
   $('.header ul.header-menu').superfish({
-    cssArrows: true
+    cssArrows: true,
+    delay: 300
+  });
+
+  $('.mobile-menu-icon').on('click', function () {
+    $(this).closest('.mobile-menu').toggleClass('mobile-menu-open');
+    $('html, body').toggleClass('open-mobile-menu');
+  });
+
+  $('.js-open-search').on('click', function (e) {
+    e.preventDefault();
+    $('.js-search-input').toggle('fast');
+  });
+
+  $('.mobile-menu-links__item').on('click', function (e) {
+    e.preventDefault();
+    $('.mobile-menu-submenu').toggleClass('open-submenu');
   });
 });
